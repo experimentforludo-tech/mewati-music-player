b7cfcb5381c057d317ab38a514d87c8fbebfee8e
experimentforludo-tech <experimentforludo@gmail.com>
2026-09-09 18:11:03 +0530

    Update eq_presets.dart

--- lib/services/eq_presets.dart
+++ lib/services/eq_presets.dart
@@ -53,7 +53,7 @@ class EqPresets {
       gains: [0, 5.3, 0, 0, 0, 0, 0, 0, 0, 0],
       bass: 0,
       width: 1,
-      truBass: 0.32,
+      truBass: 0.40,
       air: 0,
       truTreble: 0,
       advanced: true,
